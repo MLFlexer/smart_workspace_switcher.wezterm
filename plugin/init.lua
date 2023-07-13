c22a32056a52fc787140e147d4b52346176d64f0
MLFlexer <75012728+MLFlexer@users.noreply.github.com>
2023-07-13 11:15:24 +0200

    trouble shooting plugin path

--- plugin/init.lua
+++ plugin/init.lua
@@ -2,8 +2,8 @@ local wezterm = require("wezterm")
 
 wezterm.on("smart_workspace_switcher", function(window, pane)
 	local mux_window = window:mux_window()
-	local tab, tab_pane, tab_window = mux_window:spawn_tab({ args = { "../script/ws.sh" } })
-	wezterm.log_info(wezterm.plugin.list())
+	local tab, tab_pane, tab_window = mux_window:spawn_tab({ args = { "ls | fzf" } })
+	wezterm.log_info(wezterm.executable_dir)
 end)
 
 local M = {}
