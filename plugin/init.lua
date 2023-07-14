c199b9bf70629ef78d33510387b470e941d325f7
MLFlexer <75012728+MLFlexer@users.noreply.github.com>
2023-07-14 12:22:24 +0200

    renamed script, add functionality

--- plugin/init.lua
+++ plugin/init.lua
@@ -2,8 +2,7 @@ local wezterm = require("wezterm")
 
 wezterm.on("smart_workspace_switcher", function(window, pane)
 	local mux_window = window:mux_window()
-	local tab, tab_pane, tab_window = mux_window:spawn_tab({ args = { "ls | fzf" } })
-	wezterm.log_info(wezterm.executable_dir)
+	local tab, tab_pane, tab_window = mux_window:spawn_tab({ args = { "ws" } })
 end)
 
 local M = {}
